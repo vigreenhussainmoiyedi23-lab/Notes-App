ff7e2f5e89876ea9faa342461e1f1662ba4f3ba5
vigreenhussainmoiyedi23-lab <vigreenhussainmoiyedi23@gmail.com>
2026-04-07 00:47:39 +0530

    Integrated frontend to backend with build

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,11 @@ const express = require("express")
 const app = express()
 const noteModel = require("./model/notes.model")
 const cors=require("cors")
-
+const path=require("path")
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(express.static("./public"))
 
 
 
@@ -48,6 +48,10 @@ app.delete("/api/notes/:id", async (req, res) => {
         message: "note deleted successfully",
         note
     })
+})
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","public/index.html"))
 })
 
 module.exports = app
